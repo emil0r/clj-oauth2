f8b794a2f7276bc54ada105f927f112c2c5abd8c
Emil Bengtsson <emil@emil0r.com>
2020-10-30 13:57:16 +0100

    Update version and deps
    The dependencies were starting to get a little bit old. Updated to
    newer versions. Clojure is now 1.8.0, which covers most of what anyone
    actually uses.

--- project.clj
+++ project.clj
@@ -1,16 +1,21 @@
 (def dev-dependencies
-  '[[ring "1.1.5"]])
+  '[[ring "1.8.0"]])
 
-(defproject emil0r/clj-oauth2 "0.5.3"
+(defproject emil0r/clj-oauth2 "0.6.0"
+
   :description "clj-http and ring middlewares for OAuth 2.0 [fork]"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cheshire "5.0.2"]
-                 [clj-http "0.5.6"]
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [cheshire "5.10.0"]
+                 [clj-http "3.10.3"]
                  [uri "1.1.0"]
-                 [commons-codec/commons-codec "1.6"]]
+                 [commons-codec/commons-codec "1.15"]]
+
   :exclusions   [org.clojure/clojure-contrib]
+
   :dev-dependencies ~dev-dependencies
+
   :profiles {:dev {:dependencies ~dev-dependencies}}
-  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
+
   :aot [clj-oauth2.OAuth2Exception
         clj-oauth2.OAuth2StateMismatchException])
